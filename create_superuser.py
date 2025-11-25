5cb2e3d6610293b2d7868d49ed416426df035b01
Lavenderlayne <evgentimcuk12@gmail.com>
2025-11-26 00:07:26 +0200

    Update DJANGO_SETTINGS_MODULE in create_superuser script to match project name

--- create_superuser.py
+++ create_superuser.py
@@ -2,7 +2,7 @@ import os
 import django
 from django.contrib.auth import get_user_model
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.settings") # Замініть myproject на назву вашої папки!
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "blog_app.settings")
 django.setup()
 
 User = get_user_model()
